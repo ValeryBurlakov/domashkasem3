684eafcd175257bac4d37c79a19fa1c6bb954e8f
Valery <wogl@mail.ru>
2022-11-19 01:48:47 +0300

    complete

--- 23zadacha/Program.cs
+++ 23zadacha/Program.cs
@@ -1,4 +1,4 @@
-﻿// на вход число, на выход спискок кубов от 1 до n. 1 8 27 64 125
+﻿// на вход число, на выход спискок 3 степени числа от 1 до n. 1 8 27 64 125
 Console.WriteLine("Введите число");
 int x = Convert.ToInt32(Console.ReadLine());
 
@@ -7,3 +7,16 @@ for (int i = 1; i <= x; i++)
     // Console.Write(i * i * i + " ");
     Console.WriteLine(i + " в 3 степени = " + Math.Pow(i, 3));
 }
+
+
+// возведение в любую степень
+// Console.WriteLine("Введите число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Степень, в которую хотите возвести: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= x; i++)
+// {
+//     // Console.Write(i * i * i + " ");
+//     Console.WriteLine(i + $" в {y} степени = " + Math.Pow(i, y));
+// }
